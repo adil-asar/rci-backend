8a3727a92b513ea6261ca49958bdfccd35edb0b8
Adil-Ir <dev.adil@irsolutions.tech>
2025-07-15 23:30:25 +0500

    done

--- server.js
+++ server.js
@@ -8,15 +8,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })
 );
-const corsOptions = {
-  origin: ["http://localhost:5173", "https://dev.replicacopyindustries.com"],
+app.use(cors({
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true, // <== Important if using cookies or auth headers
-};
-
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); 
+}));
 
 connectDatabase();
 app.get("/", (req, res) => {
@@ -25,7 +21,7 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoute);
 app.use("/api/orders", orderRoute);
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
